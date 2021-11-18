63e69dd8840ad41b68426c39dfb9451c2d3ce6ae
Alabhya Pandey <alabhya@effectiv.ai>
2021-11-19 00:06:30 +0530

    feat: added music cover component

---
+++ src/components/MusicCover/index.tsx
@@ -0,0 +1,75 @@
+import IconButton from "../../components/IconButton";
+import React, { memo, useState } from "react";
+import {
+  Container,
+  CoverArt,
+  CoverSection,
+  SongInfoSection,
+  SongStatus,
+  SongTitle,
+  ArtistName,
+  AlbumName,
+  ActionButtons,
+  StyledFavouriteIcon,
+  StyledPlaylistAddIcon,
+  StyledShareIcon,
+} from "./styles";
+
+interface IMusicCoverProps {
+  coverArtSrc: string;
+  songStatus: string;
+  songTitle: string;
+  artistName: string;
+  albumName: string;
+}
+
+function MusicCover({
+  coverArtSrc,
+  songStatus,
+  songTitle,
+  artistName,
+  albumName,
+}: IMusicCoverProps) {
+  const [isFavouriteActive, setFavouriteActive] = useState<boolean>(false);
+  const [isPlaylistAddActive, setPlaylistAddActive] = useState<boolean>(false);
+  const [isShareActive, setShareActive] = useState<boolean>(false);
+
+  const favouriteHandler = () => {
+    setFavouriteActive((prevState) => !prevState);
+  };
+
+  const playlistAddHandler = () => {
+    setPlaylistAddActive((prevState) => !prevState);
+  };
+
+  const shareHandler = () => {
+    setShareActive((prevState) => !prevState);
+  };
+
+  return (
+    <Container>
+      <CoverSection>
+        <CoverArt src={coverArtSrc} />
+      </CoverSection>
+      <SongInfoSection>
+        <SongStatus>{songStatus}</SongStatus>
+        <SongTitle>{songTitle}</SongTitle>
+        <ArtistName>{artistName}</ArtistName>
+        <AlbumName>{albumName}</AlbumName>
+        <ActionButtons>
+          <IconButton onClickHandler={favouriteHandler}>
+            <StyledFavouriteIcon active={isFavouriteActive} />
+          </IconButton>
+          <IconButton onClickHandler={playlistAddHandler}>
+            <StyledPlaylistAddIcon active={isPlaylistAddActive} />
+          </IconButton>
+          <IconButton onClickHandler={shareHandler}>
+            <StyledShareIcon active={isShareActive} />
+          </IconButton>
+        </ActionButtons>
+      </SongInfoSection>
+    </Container>
+  );
+}
+
+export default memo(MusicCover);
---
+++ src/components/MusicCover/styles.tsx
@@ -0,0 +1,136 @@
+import styled from "styled-components";
+
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
+import ShareIcon from "@mui/icons-material/Share";
+
+interface IIconProps {
+  active?: boolean;
+}
+
+export const Container = styled.div`
+  width: 44vw;
+  height: 361px;
+  background: #f8f8f8;
+  border: 1px solid #ffffff;
+  box-sizing: border-box;
+  box-shadow: 40px 30px 80px rgba(54, 48, 116, 0.3);
+  border-radius: 60px;
+  display: flex;
+  padding: 50px;
+  z-index: 2;
+`;
+
+export const CoverSection = styled.div`
+  width: calc(50%);
+  height: calc(100%);
+  background: #f8f8f8;
+  border-radius: 40px;
+  align-self: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: inset 10px 10px 10px rgba(0, 0, 0, 0.05),
+    inset -10px -10px 20px #ffffff;
+  filter: drop-shadow(-10px -10px 15px #ffffff)
+    drop-shadow(10px 10px 10px rgba(0, 0, 0, 0.05));
+`;
+
+export const CoverArt = styled.img`
+  width: calc(100% - 30px);
+  height: calc(100% - 30px);
+  border-radius: 30px;
+  box-shadow: inset 10px 10px 15px rgba(255, 255, 255, 0.2);
+  filter: drop-shadow(5px 5px 10px rgba(37, 5, 57, 0.51));
+`;
+
+export const SongInfoSection = styled.div`
+  width: calc(50% - 45px);
+  padding: 0 45px;
+`;
+
+export const SongStatus = styled.div`
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 21px;
+  display: flex;
+  align-items: center;
+  font-feature-settings: "liga" off;
+
+  color: #aeaeae;
+
+  text-shadow: 1px 1px 1px #ffffff;
+  margin-bottom: 40px;
+  margin-top: 16px;
+`;
+
+export const SongTitle = styled.div`
+  font-style: normal;
+  font-weight: 900;
+  font-size: 24px;
+  line-height: 24px;
+  display: flex;
+  align-items: center;
+  font-feature-settings: "liga" off;
+
+  color: #86748d;
+  margin-bottom: 16px;
+`;
+
+export const ArtistName = styled.div`
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 20px;
+  display: flex;
+  align-items: center;
+  font-feature-settings: "liga" off;
+
+  color: #bfb9be;
+  margin-bottom: 16px;
+`;
+
+export const AlbumName = styled.div`
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 18px;
+  display: flex;
+  align-items: center;
+  font-feature-settings: "liga" off;
+
+  color: #bfb8be;
+  margin-bottom: 34px;
+`;
+
+export const ActionButtons = styled.div`
+  display: flex;
+`;
+
+export const StyledFavouriteIcon = styled(FavoriteIcon)<IIconProps>`
+  cursor: pointer;
+  &:hover {
+    transform: scale(1.1);
+    transition: transform 500ms ease-in;
+  }
+  color: ${({ active }) => (active ? `#5F30C1` : `#c7c5d0`)};
+`;
+
+export const StyledPlaylistAddIcon = styled(PlaylistAddIcon)<IIconProps>`
+  color: ${({ active }) => (active ? `#5F30C1` : `#c7c5d0`)};
+  cursor: pointer;
+  &:hover {
+    transform: scale(1.1);
+    transition: transform 500ms ease-in;
+  }
+`;
+
+export const StyledShareIcon = styled(ShareIcon)<IIconProps>`
+  color: ${({ active }) => (active ? `#5F30C1` : `#c7c5d0`)};
+  cursor: pointer;
+  &:hover {
+    transform: scale(1.1);
+    transition: transform 500ms ease-in;
+  }
+`;
